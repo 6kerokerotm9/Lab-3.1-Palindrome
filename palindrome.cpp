86a80a1a349a7966788ee173ccd636f6c4a6b638
6kerokerotm9 <luckystripes60@gmail.com>
2018-11-05 21:12:44 -0800

    11/05 21:12
    Finished product.

--- palindrome.cpp
+++ palindrome.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include <cctype>
 
 struct node {
   char letter;
@@ -13,19 +14,8 @@ bool isEmpty(int counter) {
   return false;
 }
 
-bool isFull(int counter, int size) {
-  if(counter == size) { //checks to see if full, might not be used
-    return false;
-  }
-  return false;
-}
-
-
-void push(node * &n, int &counter, int size, char input) { //can be used for both stack and queue because of how pops work
-  if(isFull(counter, size) == true) {
-    return;
-  }
-  else if(isEmpty(counter) == false) { //for every push after the first case
+void push(node * &n, int &counter, char input) { //can be used for both stack and queue because of how pops work
+  if(isEmpty(counter) == false) { //for every push after the first case
     n = n->next; 
   }
   n->letter = input;
@@ -50,9 +40,23 @@ char pop_queue(node * &head, int &counter) {
   if(isEmpty(counter) == true) {
     return ' ';
   }
-  char temp = head->letter;
-  head = head->next;
+  char temp = head->letter; 
+  head = head->next; //sets the first elements to the next element
   return temp;
+}
+
+void fill(node * &current, std::string s, int &counter) { //function to fill the queue and stack
+  for(int i=0; i < s.size(); i++) { //for loop that takes character and puts it in each place in the stack or queue
+    if(s[i] < 91 && s[i] > 64) { //checks to see if the letter is uppercase
+      push(current, counter, std::tolower(s[i])); //changes the letter to lowercase and then pushes it in
+    }
+    else if(s[i] < 97 || s[i] > 122) { //if the letter is a symbol then this catches it 
+      continue; //ignore all the symbols, spaces, and numbers and do not push into the stack for the sake of simplicity
+    }
+    else { 
+      push(current, counter, s[i]);
+    }
+  }
 }
 
 void print(node * head, int counter) { //test print statement
@@ -60,36 +64,42 @@ void print(node * head, int counter) { //test print statement
     std::cout << head->letter << " ";
     head = head->next;
   }
+  std::cout << std::endl;
 }
 
 int main() {
-  //initialize stacks
-  node * head_stack = new node;
-  node * current_stack = new node;  
+  //initialize stack
+  node * head_stack = new node; //creates the head pointer
+  node * current_stack = new node; //keeps track of the most recently added element into the stack
   head_stack = current_stack;
-  int size_stack = 10;
-  int counter_stack = 0;
+  int counter_stack = 0; //counter for stack to keep track of how many elements are in the stack
   
-  //initialize queues
-  node * head_queue = new node;
-  node * current_queue = new node; 
+  //initialize queue
+  node * head_queue = new node; //creates the queue head pointer
+  node * current_queue = new node; //same as stack but for queue
   head_queue = current_queue;
-  int size_queue = 10;
-  int counter_queue = 0;
-
-  /*
-  push(current_stack, counter_stack, size_stack, 'r');
-  push(current_stack, counter_stack, size_stack, 'o');
-  std::cout << pop_stack(current_stack, head_stack, counter_stack) << std::endl;
-  std::cout << pop_stack(current_stack, head_stack, counter_stack) << std::endl;
-  */
-
-  push(current_queue, counter_queue, size_queue, 'u');
-  push(current_queue, counter_queue, size_queue, 'm');
-  std::cout << pop_queue(head_queue, counter_queue) << std::endl;
-  std::cout << pop_queue(head_queue, counter_queue) << std::endl;
-
-
-
-
+  int counter_queue = 0; //counter for queue to see how many elements are in the queue
+  
+  //initialize other variables
+  std::string input;
+  int size; //constant that will be used later when the counters change because of popping 
+ 
+  std::cout << "Please enter a word: ";
+  std::getline(std::cin, input);
+  fill(current_stack, input, counter_stack); //fill the stack with the characters from the string
+  fill(current_queue, input, counter_queue); //fills the queue
+  size = counter_stack; //set size to how many elements are max in the queue and stack
+  print(head_stack, counter_stack);
+  print(head_queue, counter_queue);
+  for(int i=1; i<=size; i++) {
+    if(pop_stack(current_stack, head_stack, counter_stack) != pop_queue(head_queue, counter_queue)) { //pops accordingly and then compares
+      std::cout << "The input is not a palindrome." << std::endl;
+      break;
+    }
+    else if(i == size) { //if all the letters are popped out and all matched to each other
+      std::cout << "The input is a palindrome." << std::endl;
+    }
+  }   
 }
+    
+
